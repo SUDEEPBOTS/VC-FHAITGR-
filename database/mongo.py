0fdfc18cbe55ae7c8b6a5018010371cb17a3dad1
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-18 10:42:21 +0530

    Create mongo.py

---
+++ database/mongo.py
@@ -0,0 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
+
+mongo = AsyncIOMotorClient(MONGO_URL)
+db = mongo.UserbotNetwork
+sessions = db.sessions
+
+async def add_session(user_id, session_string):
+    await sessions.update_one(
+        {"user_id": user_id},
+        {"$set": {"session": session_string}},
+        upsert=True
+    )
+
+async def remove_session(user_id):
+    await sessions.delete_one({"user_id": user_id})
+
+async def get_all_sessions():
+    cursor = sessions.find({})
+    return [doc["session"] async for doc in cursor]
+  
