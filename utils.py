24bd9d0973e5a4b37767c108d77686f066507bac
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-18 10:41:42 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+def sm(text):
+    """
+    converts normal text to small caps
+    """
+    normal = "abcdefghijklmnopqrstuvwxyz"
+    small_caps = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
+    table = str.maketrans(normal, small_caps)
+    return text.lower().translate(table)
+  
