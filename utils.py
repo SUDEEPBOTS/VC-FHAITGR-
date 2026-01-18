96222fe017ad9fa632ec219cac41bbde60f14c56
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-18 10:46:03 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def sm(text):
-    """
-    converts normal text to small caps
-    """
-    normal = "abcdefghijklmnopqrstuvwxyz"
-    small_caps = "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
-    table = str.maketrans(normal, small_caps)
-    return text.lower().translate(table)
-  
